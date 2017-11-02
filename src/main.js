d174ffd27f78c7d050d925dcd906b4daf876244b
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-11-02 11:21:15 +0300

    Added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Lang from 'vuejs-localization';
 
-Vue.config.productionTip = false
+Lang.requireAll(require.context('./lang', true, /\.js$/));
+
+Vue.use(BootstrapVue);
+Vue.use(Lang);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
