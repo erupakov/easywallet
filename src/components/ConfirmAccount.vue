3855b62fd32eb075571fc52d3d4f3e284421aea1
Tarik Kumar <tarikkumar7@gmail.com>
2017-12-11 00:45:08 +0300

    Added key derivation

--- src/components/ConfirmAccount.vue
+++ src/components/ConfirmAccount.vue
@@ -14,6 +14,10 @@
 </template>
 
 <script>
+import bip39 from 'bip39'
+import bitcoinjs from 'bitcoinjs-lib'
+import ethUtil from 'ethereumjs-utils'
+
 export default {
   name: 'ConfirmAccount',
   data () {
@@ -22,7 +26,7 @@ export default {
       btn_confirm_msg: this.$lang.confirm_account.btn_confirm_text,
       btn_back_msg: this.$lang.confirm_account.btn_back_text,
       seed_phrase: ''
-    }
+    };
   },
   methods: {
     goBack: function (event) {
@@ -45,8 +49,80 @@ export default {
           text: 'Seed phrase correct, opening wallet.'
         })
 
+        var seed = null
+        var bip32RootKey = null
+        var bip32ExtendedKey = null
+        var network = bitcoinjs.bitcoin.networks.bitcoin
+        // calculate seed
+        seed = bip39.toSeed(words, null)
+        // calculate rootKey
+        bip32RootKey = bitcoinjs.bitcoin.HDNode.fromSeedHex(seed, network)
+
+        // clear words
+        this.$session.clear('mnemonicPhrase')
+        words = null
+        var bip39ExtendedKey = this.calcBip39ExtendedKey(
+          'm/44\'/60\'/0\'/0',
+          bip32RootKey
+        )
+        // TODO: calculate currect account index according BIP-44 directions
+        // on address gaping
+        // create initial wallet structure
+        var wallet = {
+          rootKey: bip32RootKey,
+          extendedKey: bip32ExtendedKey,
+          accounts: [
+            {
+              type: 0,
+              name: '',
+              password: '',
+              derivePath: '',
+              balance: ''
+            }
+          ]
+        };
+        this.$session.set('wallet', wallet)
+        this.$session.set('selectedAccountIndex', 0)
         this.$router.push('/home/name')
       }
+    },
+    calcBip32ExtendedKey: function(path, bip32RootKey) {
+      // Check there's a root key to derive from
+      if (!bip32RootKey) {
+        return bip32RootKey
+      }
+      var extendedKey = bip32RootKey
+      // Derive the key from the path
+      var pathBits = path.split('/')
+      for (var i = 0; i < pathBits.length; i++) {
+        var bit = pathBits[i]
+        var index = parseInt(bit)
+        if (isNaN(index)) {
+          continue
+        }
+        var hardened = bit[bit.length - 1] === '\'';
+        var isPriv = !extendedKey.isNeutered()
+        var invalidDerivationPath = hardened && !isPriv
+        if (invalidDerivationPath) {
+          extendedKey = null
+        } else if (hardened) {
+          extendedKey = extendedKey.deriveHardened(index)
+        } else {
+          extendedKey = extendedKey.derive(index)
+        }
+      }
+      return extendedKey
+    },
+    deriveKey: function(index, extendedKey) {
+      var key = extendedKey.derive(index)
+      var privKeyBuffer = key.keyPair.d.toBuffer()
+      var addressBuffer = ethUtil.privateToAddress(privKeyBuffer)
+      var hexAddress = addressBuffer.toString('hex')
+      var checksumAddress = ethUtil.toChecksumAddress(hexAddress)
+      var address = ethUtil.addHexPrefix(checksumAddress)
+      var privkey = ethUtil.addHexPrefix(privkey)
+      var pubkey = ethUtil.addHexPrefix(pubkey)
+      return { private: privkey, public: pubkey, address: address }
     }
   }
 }
